42f984291c1c1dc316c75db66e8bcee1831a87f2
Evan Rowley <rowley.evan@gmail.com>
2019-01-31 11:12:58 -0500

    fixed some issues in preonic rev3 atrix via code from default layout

--- keyboards/preonic/keymaps/atrix/keymap.c
+++ keyboards/preonic/keymaps/atrix/keymap.c
@@ -81,7 +81,7 @@ enum preonic_keycodes {
   NAV,
   NUM,
   FUN,
-  BRITE
+  BACKLIT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LALT, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RALT, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  BRITE,   KC_ESC,  KC_TAB,  KC_LGUI, NAV,     KC_BSPC, KC_SPC,  NUM,     KC_MINS, KC_QUOT, KC_ENT,  ___X___  \
+  BACKLIT, KC_ESC,  KC_TAB,  KC_LGUI, NAV,     KC_BSPC, KC_SPC,  NUM,     KC_MINS, KC_QUOT, KC_ENT,  ___X___  \
 ),
 
 /* Colemak
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LALT, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_RALT, \
   KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_RCTL, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  BRITE, KC_ESC,  KC_TAB,  KC_LGUI, NAV,     KC_BSPC, KC_SPC,  NUM,     KC_MINS, KC_QUOT, KC_ENT,  ___X___  \
+  BACKLIT, KC_ESC,  KC_TAB,  KC_LGUI, NAV,     KC_BSPC, KC_SPC,  NUM,     KC_MINS, KC_QUOT, KC_ENT,  ___X___  \
 ),
 
 /* Dvorak
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LALT, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_RALT, \
   KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_RCTL, \
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-  BRITE, KC_ESC,  KC_TAB,  KC_LGUI, NAV,     KC_BSPC, KC_SPC,  NUM,     KC_MINS, KC_SLSH, KC_ENT,  ___X___  \
+  BACKLIT, KC_ESC,  KC_TAB,  KC_LGUI, NAV,     KC_BSPC, KC_SPC,  NUM,     KC_MINS, KC_SLSH, KC_ENT,  ___X___  \
 ),
 
 /* Shifted
@@ -276,19 +276,110 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
           break;
-        case BRITE:
+        case BACKLIT:
           if (record->event.pressed) {
             register_code(KC_RSFT);
             #ifdef BACKLIGHT_ENABLE
               backlight_step();
             #endif
+            #ifdef __AVR__
             PORTE &= ~(1<<6);
+            #endif
           } else {
             unregister_code(KC_RSFT);
+            #ifdef __AVR__
             PORTE |= (1<<6);
+            #endif
           }
-          return false;
+           return false;
           break;
       }
     return true;
 };
+
+bool muse_mode = false;
+uint8_t last_muse_note = 0;
+uint16_t muse_counter = 0;
+uint8_t muse_offset = 70;
+uint16_t muse_tempo = 50;
+
+void encoder_update(bool clockwise) {
+  if (muse_mode) {
+    if (IS_LAYER_ON(_NUM)) {
+      if (clockwise) {
+        muse_offset++;
+      } else {
+        muse_offset--;
+      }
+    } else {
+      if (clockwise) {
+        muse_tempo+=1;
+      } else {
+        muse_tempo-=1;
+      }
+    }
+  } else {
+    if (clockwise) {
+      register_code(KC_PGDN);
+      unregister_code(KC_PGDN);
+    } else {
+      register_code(KC_PGUP);
+      unregister_code(KC_PGUP);
+    }
+  }
+}
+
+void dip_update(uint8_t index, bool active) {
+  switch (index) {
+    case 0:
+      if (active) {
+        layer_on(_FUN);
+      } else {
+        layer_off(_FUN);
+      }
+      break;
+    case 1:
+      if (active) {
+        muse_mode = true;
+      } else {
+        muse_mode = false;
+        #ifdef AUDIO_ENABLE
+          stop_all_notes();
+        #endif
+      }
+   }
+}
+
+/*
+ * I took this out because it gives me this compile error:
+ *
+ * error: implicit declaration of function 'muse_clock_pulse' [-Werror=implicit-function-declaration]
+ *  uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+ *                                          ^~~~~~~~~~~~~~~~
+ *
+void matrix_scan_user(void) {
+  #ifdef AUDIO_ENABLE
+    if (muse_mode) {
+      if (muse_counter == 0) {
+        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+        if (muse_note != last_muse_note) {
+          stop_note(compute_freq_for_midi_note(last_muse_note));
+          play_note(compute_freq_for_midi_note(muse_note), 0xF);
+          last_muse_note = muse_note;
+        }
+      }
+      muse_counter = (muse_counter + 1) % muse_tempo;
+    }
+  #endif
+}
+*/
+
+bool music_mask_user(uint16_t keycode) {
+  switch (keycode) {
+    case NUM:
+    case NAV:
+      return false;
+    default:
+      return true;
+  }
+}
